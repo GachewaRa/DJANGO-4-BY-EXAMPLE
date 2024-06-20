7f9560f052d6d07ea19c73c641a33d7cc950f395
GachewaRa <gachewaadrian@gmail.com>
2024-06-20 22:12:28 +0300

    Implemented Actions and the action stream, yet to test because of an unresponsive DOM

--- Django-5/bookmarks/account/views.py
+++ Django-5/bookmarks/account/views.py
@@ -6,14 +6,28 @@ from .forms import LoginForm,  UserRegistrationForm, \
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
-
+from actions.utils import create_action
+from actions.models import Action
 
 # Create your views here.
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', 
-                  {'section': 'dashboard'})
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    
+    actions = actions.select_related('user', 'user__profile')\
+        .prefetch_related('target')[:10]
+    
+    return render(request, 
+                  'account/dashboard.html', 
+                  {'section': 'dashboard', 'actions': actions})
+
 
 
 def user_login(request):
@@ -49,6 +63,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,'account/register_done.html',
                           {'new_user': new_user})
     else:
@@ -119,6 +134,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({'status': 'ok'})
