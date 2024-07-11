a9ca4fbc88da621c4a5243231e9b6f30c4c5f6c3
GachewaRa <gachewaadrian@gmail.com>
2024-07-11 23:12:33 +0300

    Set up local and production environments; set up the requirements as well as Docker and Docker Compose

---
+++ ELEARNING/educa/educa/settings/prod.py
@@ -0,0 +1,15 @@
+from .base import *
+
+#call CUY3BGcCb58yMwf
+
+DEBUG = False
+
+ADMINS = [
+    ('Gachewa A', 'gachewaadrian@gmail.com'),
+]
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+    }
+}
